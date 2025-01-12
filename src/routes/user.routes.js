4459758c56e0e355698d4e5de80e904d3475aaf2
Paras Nath <parasnathchauhan@gmail.com>
2025-01-13 03:54:16 +0530

    Add user account features: change password, update details, and retrieve user profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,14 @@ import {
     loginUser, 
     logoutUser,
     registerUser, 
-   refreshAccessToken
+   refreshAccessToken,
+   changeCurrentPassword,
+   updateUserCoverImage,
+   updateAccountDetails,
+   updateAvatarImage,
+   getCurrentUser,
+   getUserChannelProfile
+
    
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
@@ -32,6 +39,11 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-
-
+router.route("/change-password").post(verifyJWT,  changeCurrentPassword)
+router.route("/get-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatarImage)
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/get-user-channel/:userame").get(verifyJWT, getUserChannelProfile)
+router.route("/get-watchHistory").get(verifyJWT, getWatchHistory)
 export default router
